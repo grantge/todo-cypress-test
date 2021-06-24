de5c291170b45f7425b0e9dbe96bf52d2934e977
grantge <grantfromgeorgia@gmail.com>
2021-06-24 19:54:19 +0300

    add post request

--- public/script/index.js
+++ public/script/index.js
@@ -1,15 +1,23 @@
 const field = document.querySelector('.column-2');
 
+const todoName = document.querySelector('.todoName');
+const todoDescription = document.querySelector('.descriptionName');
+
+const btn = document.querySelector('.btn');
+
 // REST API
 
-axios
-  .get('http://localhost:4000/api/posts')
-  .then((res) => {
-    console.log(res.data);
-    const posts = res.data;
+// functions
 
-    posts.forEach((todos) => {
-      field.innerHTML += `
+function getReq() {
+  axios
+    .get('http://localhost:4000/api/posts')
+    .then((res) => {
+      console.log(res.data);
+      const posts = res.data;
+
+      posts.forEach((todos) => {
+        field.innerHTML += `
                 <div class="ui card message" style="width: 440px">
                 <i class="close icon btn" onclick="activateModal()"></i>
                 <i
@@ -27,8 +35,36 @@ axios
                 </div>
               </div>
       `;
+      });
+    })
+    .catch((error) => {
+      console.log(error);
     });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+}
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  getReq();
+  // POST request
+
+  axios
+    .post('/api/posts', {
+      title: `${todoName.value}`,
+      time:
+        new Date().getDay() +
+        '.' +
+        (new Date().getMonth() + 1) +
+        '.' +
+        new Date().getFullYear(),
+      description: `${todoDescription.value}`,
+    })
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+getReq();
